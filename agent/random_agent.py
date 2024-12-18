37de3323e88f0c5afea32e19a4df33fdb8bd581f
CuWO4 <wutong.tony@outlook.com>
2024-12-18 18:51:34 +0800

    prof: try random actions first and return the first legal one. after failing several times, randomly select from all legal actions as before. optimized by 3~10x depending on board size.

--- agent/random_agent.py
+++ agent/random_agent.py
@@ -2,6 +2,7 @@ from .base import Agent
 
 import go.goboard as goboard
 from utils.eye_identifier import is_point_an_eye
+from utils.move_idx_transformer import idx_to_move
 
 import random
 
@@ -14,6 +15,17 @@ class RandomAgent(Agent):
     super().__init__(need_move_queue=need_move_queue, need_mcts_queue=need_mcts_queue)
   
   def select_move(self, game_state: goboard.GameState) -> goboard.Move:
+
+    # try random actions first and return the first legal one
+    # after failing several times, randomly select from all legal actions
+    board_size = game_state.board.num_rows * game_state.board.num_cols
+    for _ in range(board_size // 4):
+      rand_idx = random.randrange(0, board_size)
+      rand_move = idx_to_move(rand_idx, game_state.board.size)
+      if game_state.is_valid_move(rand_move) \
+        and not is_point_an_eye(game_state.board, rand_move.point, game_state.next_player):
+        return rand_move
+
     candidates = []
     for move in game_state.legal_moves():
       if not move.is_pass and \
