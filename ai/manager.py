b3b5cd511d8a3dc5f20efc422c62f6c8de5787eb
CuWO4 <wutong.tony@outlook.com>
2025-04-05 15:44:08 +0800

    refactor: decoupling the model / meta data / summary writer loading

--- ai/manager.py
+++ ai/manager.py
@@ -5,6 +5,7 @@ import os
 import json
 import torch
 from torch.utils.tensorboard import SummaryWriter
+import contextlib
 
 __all__ = [
   'create',
@@ -19,61 +20,78 @@ META_DATA_FILE = 'meta.json'
 
 def create(ModelType: type, model_params: dict, path: str, dumb_input: torch.Tensor) -> None:
   os.makedirs(path)
-  
+
   model = ModelType(**model_params)
   model_file = os.path.join(path, MODEL_FILE)
   torch.save(model.state_dict(), model_file)
-  
+
   model_json = os.path.join(path, MODEL_SETTING_FILE)
   with open(model_json, "w") as f:
     json.dump(model_params, f)
-  
+
   tb_dir = os.path.join(path, TENSORBOARD_DIR)
   os.makedirs(tb_dir, exist_ok=True)
   writer = SummaryWriter(tb_dir)
-  
+
   model = model.cpu()
   dumb_input = dumb_input.cpu()
   writer.add_graph(model, dumb_input)
   writer.close()
-  
+
   meta = MetaData()
   meta_json = os.path.join(path, META_DATA_FILE)
   with open(meta_json, "w") as f:
     json.dump(meta, f, cls=GeneralObjectEncoder)
-    
+
   print('model successfully created')
 
-def save(model: torch.nn.Module, meta: MetaData, path: str):
+def save(model: torch.nn.Module, path: str):
   model_file = os.path.join(path, MODEL_FILE)
   torch.save(model.state_dict(), model_file)
-  
-  meta_json = os.path.join(path, META_DATA_FILE)
-  with open(meta_json, "w") as f:
-    json.dump(meta, f, cls=GeneralObjectEncoder)
-    
+
   print('model successfully saved')
 
 def load(
-  ModelType: type, path: str, 
+  ModelType: type, path: str,
   device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
-) -> tuple[torch.nn.Module, MetaData, SummaryWriter]:
+) -> torch.nn.Module:
   model_json = os.path.join(path, MODEL_SETTING_FILE)
   with open(model_json, "r") as f:
     model_params = json.load(f)
-  
+
   model = ModelType(**model_params)
   model_file = os.path.join(path, MODEL_FILE)
   model.load_state_dict(torch.load(model_file))
   model = model.to(device=device)
-  
+
+  print('model successfully loaded')
+
+  return model
+
+def save_meta(meta: MetaData, path: str):
+  meta_json = os.path.join(path, META_DATA_FILE)
+  with open(meta_json, "w") as f:
+    json.dump(meta, f, cls=GeneralObjectEncoder)
+
+  print('meta of model successfully saved')
+
+def load_meta(path: str) -> MetaData:
   meta_json = os.path.join(path, META_DATA_FILE)
   with open(meta_json, "r") as f:
     meta = json.load(f, object_hook=general_object_hook)
-  
+
+  print('meta of model successfully loaded')
+
+  return meta
+
+@contextlib.contextmanager
+def load_summary_writer(path: str) -> SummaryWriter:
   tb_dir = os.path.join(path, TENSORBOARD_DIR)
   writer = SummaryWriter(tb_dir)
+  print('summary writer successfully loaded')
   
-  print('model successfully loaded')
-  
-  return model, meta, writer
+  try:
+    yield writer
+  finally:
+    writer.close()
+    print('summary writer closed')
