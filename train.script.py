bd0d404a4b76c4b5d99e0b57c8f0ac19576086e6
CuWO4 <wutong.tony@outlook.com>
2025-05-09 15:16:17 +0800

    conf: introduce configurable system in replace of cmd line parameters

--- train.script.py
+++ train.script.py
@@ -1,30 +1,13 @@
 import argparse
+import json
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-m', '--model', type = str, required = True, help = 'root to model')
-  parser.add_argument('--batch-size', type = int, default = 128)
-  parser.add_argument('--batch-accumulation', type = int, default = 1,
-                      help = 'equivalent virtual batch size = batch size * batch accumulation')
-  # supervisor learning does not requires very large batch size
+  parser.add_argument('-c', '--conf', type = str, required = True, help = 'path to configuration')
   parser.add_argument('-d', '--dataset', type = str, required = True, help = 'root to dataset')
-  parser.add_argument('--batch-per-test', type = int, default = 100)
-  parser.add_argument('--test-dataset', type = str, help = 'root to test dataset. if not given, '
-                      'test phase is skipped')
-  parser.add_argument('--prefetch-batch', type = int, default = 300)
-  parser.add_argument('--test-prefetch-batch', type = int, default = 20)
-  parser.add_argument('--lr', type = float, default = 0.1, help = 'base lr')
-  parser.add_argument('--weight-decay', type = float, default = 1e-4)
-  parser.add_argument('--momentum', type = float, default = 0.9)
-  parser.add_argument('--gradient-clip', type = float, default = 2.0)
-  parser.add_argument('--policy-loss-weight', type = float, default = 0.85)
-  parser.add_argument('--value-loss-weight', type = float, default = 0.03)
-  parser.add_argument('--soft-target-nominal-weight', type = float, default = 2.0,
-                      help = 'nominal weight of soft policy target')
-  parser.add_argument('--softening-intensity', type = float, default = 1 / 4,
-                      help = 'softening temperature / original temperature')
-  parser.add_argument('--checkpoint-interval-sec', type = int, default = 3600, help = 'seconds '
-                      'between two checkpoints saved')
+  parser.add_argument('-t', '--test-dataset', type = str, help = 'root to test dataset.'
+                      'if not given, test phase is skipped')
   args = parser.parse_args()
 
   # importing includes torch, slow.
@@ -38,39 +21,28 @@ def main():
 
   import torch.optim as optim
 
+  with open(args.conf, 'r') as config_file:
+    config = json.load(config_file)
+
   trainer = Trainer(
     model_manager = ModelManager(args.model, ZhuGo),
-    optimizer_manager = OptimizerManager(
-      args.model, optim.SGD,
-      optim_kwargs = {
-        "lr": args.lr,
-        "weight_decay": args.weight_decay,
-        "momentum": args.momentum,
-      }
-    ),
+    optimizer_manager = OptimizerManager(args.model, optim.SGD, optim_kwargs = config['optimizer']),
     dataloader = BGTFDataLoader(
       args.dataset,
-      args.batch_size,
+      config['batch_size'],
       ZhuGoEncoder(device = 'cpu'), # prefetched tensors saved on cpu
       debug = True,
-      prefetch_batch = args.prefetch_batch,
+      prefetch_batch = config['prefetch_batch'],
     ),
-    batch_accumulation = args.batch_accumulation,
-    batch_per_test = args.batch_per_test,
     test_dataloader = BGTFDataLoader(
       args.test_dataset,
-      args.batch_size,
+      config['batch_size'],
       ZhuGoEncoder(device = 'cpu'), # prefetched tensors saved on cpu
-      prefetch_batch = args.test_prefetch_batch,
+      prefetch_batch = config['test_prefetch_batch'],
     )
       if args.test_dataset is not None
       else None,
-    gradient_clip = args.gradient_clip,
-    policy_loss_weight = args.policy_loss_weight,
-    value_loss_weight = args.value_loss_weight,
-    soft_target_nominal_weight = args.soft_target_nominal_weight,
-    softening_intensity = args.softening_intensity,
-    checkpoint_interval_sec = args.checkpoint_interval_sec,
+    **config['trainer'],
   )
 
   trainer.train()
