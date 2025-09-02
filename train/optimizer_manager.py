5e10d9551f4fcad36d01ac3cace7e9c369d5968f
CuWO4 <wutong.tony@outlook.com>
2025-09-02 15:52:08 +0800

    refactor: refactor the interface of optimizer manager

--- train/optimizer_manager.py
+++ train/optimizer_manager.py
@@ -1,14 +1,19 @@
+from ai.zhugo import ZhuGo
+
 import torch
 import torch.optim as optim
 import os
-from torch.nn.parameter import Parameter
-from typing import Iterable
+
+__all__ = [
+  'OptimizerManager'
+]
+
 
 class OptimizerManager:
   OPTIMIZER_FILE = 'optimize.pth'
 
   def __init__(
-    self, root: str, OptimizerType: type, 
+    self, root: str, OptimizerType: type,
     *, optim_args: tuple = (), optim_kwargs: dict = {},
   ):
     self.root = root
@@ -21,17 +26,17 @@ class OptimizerManager:
     torch.save(optimizer.state_dict(), optimizer_path)
     print('optimizer saved successfully')
 
-  def load_optimizer(self, model_parameters: Iterable[Parameter]):
+  def load_optimizer(self, model: ZhuGo):
     optimizer: optim.Optimizer = self.OptimizerType(
-      model_parameters, *self.optim_args, **self.optim_kwargs
+      model.parameters(), *self.optim_args, **self.optim_kwargs
     )
 
     optimizer_path = os.path.join(self.root, self.OPTIMIZER_FILE)
-    if os.path.exists(optimizer_path):
+    try:
       state_dict = torch.load(optimizer_path)
       optimizer.load_state_dict(state_dict)
       print('optimizer loaded successfully')
-    else:
-      print('failed to load optimizer state, use uninitialized optimizer instead')
+    except Exception as e:
+      print(f'failed to load optimizer state, use uninitialized optimizer instead (`{e}`)')
 
     return optimizer
